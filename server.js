172a9a2206b9895031798ffeeb8bb3e4c6881877
AvellPessoal <lincoln.araujo.dias@gmail.com>
2020-10-06 18:15:28 -0300

    add procfile

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ require('dotenv').config()
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/client-web'));
+app.use(express.static(__dirname + '/dist/client-web'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join('./dist/client-web/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/client-web/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+    console.log("rodando no servidor porta:" + process.env.PORT)
+})
