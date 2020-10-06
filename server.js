c72ac8ae17826c547ce3c20fe9fcc50b7efe187d
AvellPessoal <lincoln.araujo.dias@gmail.com>
2020-10-06 16:27:36 -0300

    removing string off server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/client-web'));
-zz
+
 app.get('/*', function(req, res) {
 
     res.sendFile(path.join(__dirname + '/dist/client-web/index.html'));
