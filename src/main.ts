89ae18c34da7cd3c6a5dece6bfafa670fab0077e
AvellPessoal <lincoln.araujo.dias@gmail.com>
2020-10-06 22:31:10 -0300

    start dotenv maint.js

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import dotenv from 'dotenv'
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 require('dotenv').config()
-dotenv.config()
+
 if (environment.production) {
   enableProdMode();
 }
